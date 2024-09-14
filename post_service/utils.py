d0e6f03691ee6609baf33ccf5c2926e8d5ba4df1
Danila Tarasov <rost6os@gmail.com>
2024-09-14 09:29:40 +0300

    set verification letter ru

--- post_service/utils.py
+++ post_service/utils.py
@@ -1,22 +1,21 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 from config import POST_USER, POST_PWD
 
 
 def verify_email(user, code):
     login = POST_USER
     password = POST_PWD
-    sent_from = login
     to = user
-    subject = "Email Verification on LazyLearn Academy"
-    body = f"Your verification code: {code}"
-    email_text = """
-    From: %s
-    To: %s
-    Subject: %s
-    %s
-    """ % (sent_from, to, subject, body)
+    subject = "Верификация в LazyLearn Academy"
+    body = f"Ваш код подтверждения: {code}"
+    msg = MIMEText(body, 'plain', 'utf-8')
+    msg['Subject'] = Header(subject, 'utf-8')
+    msg['From'] = login
+    msg['To'] = to
     smtp_server = smtplib.SMTP_SSL("smtp.timeweb.ru", 465)
     smtp_server.ehlo()
     smtp_server.login(login, password)
-    smtp_server.sendmail(sent_from, to, email_text)
+    smtp_server.sendmail(login, to, msg.as_string())
     smtp_server.close()
